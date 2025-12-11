18eaed07a2df3406708ffe25b8553d7c27b04e10
dido1043 <dudunovski_05@abv.bg>
2025-12-11 20:16:06 +0200

    up

---
+++ AssetFlowApi/src/main/java/org/af/assetflowapi/AssetFlowApiApplication.java
@@ -0,0 +1,13 @@
+package org.af.assetflowapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetFlowApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssetFlowApiApplication.class, args);
+    }
+
+}
